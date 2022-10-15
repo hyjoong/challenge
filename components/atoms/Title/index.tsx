885cc1986ecfef774b8fee2fb48ef8d5b5fdca49
hyjoong <maplesosser@naver.com>
2022-10-16 03:19:14 +0900

    feat: title 컴포넌트 구현

---
+++ components/atoms/Title/index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import styled from "styled-components";
+
+export interface Props {
+  children: React.ReactNode;
+}
+
+const Title = ({ children }: Props) => {
+  return <StyledTitle>{children}</StyledTitle>;
+};
+
+const StyledTitle = styled.p`
+  font-size: 15px;
+  font-weight: 600;
+  color: ${({ theme }) => theme.color.skyBlue};
+`;
+
+export default Title;
