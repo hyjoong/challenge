ee8a9b7c7b14c3cc32682f9f70875e4da8b4bc7e
hyjoong <maplesosser@naver.com>
2022-10-16 14:25:48 +0900

    feat: innerBox 컴포넌트에 tab 컴포넌트 적용

--- components/templates/InnerBox/index.tsx
+++ components/templates/InnerBox/index.tsx
@@ -1,21 +1,42 @@
 import React from "react";
 import styled from "styled-components";
+import { useRouter } from "next/router";
+import Tab from "components/atoms/Tab";
 import Header from "components/molecules/Header";
+import { tabList } from "constants/index";
 
 interface Props {
   children?: React.ReactNode;
 }
 
 const InnerBox = ({ children }: Props) => {
+  const router = useRouter();
+  const handleTabClick = (e: React.MouseEvent<HTMLButtonElement>): void => {
+    const { value } = e.target as HTMLButtonElement;
+    router.push(`/${value}`);
+  };
+
   return (
     <StyledInnerBox>
       <Header />
       <Container>{children}</Container>
+      {tabList.map((tab, key) => (
+        <Tab
+          key={key}
+          index={key}
+          value={tab.value}
+          onClick={handleTabClick}
+          isActive={router.pathname === tab.value}
+        >
+          {tab.title}
+        </Tab>
+      ))}
     </StyledInnerBox>
   );
 };
 
 const StyledInnerBox = styled.div`
+  position: relative;
   display: flex;
   flex-direction: column;
   width: 748px;
