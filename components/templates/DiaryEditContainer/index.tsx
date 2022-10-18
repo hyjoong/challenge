5696a93ecebec2be3ea7a5ddd408b100b17979ed
hyjoong <maplesosser@naver.com>
2022-10-18 15:27:47 +0900

    fix typo

--- components/templates/DiaryEditContainer/index.tsx
+++ components/templates/DiaryEditContainer/index.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import DiaryNewWrapper from "components/organisms/DiaryNewWrapper";
 import Profile from "components/organisms/Profile";
 import InnerBox from "../InnerBox";
-import { useUpdateeBoardMutation } from "lib/graphql/queries/schema";
+import { useUpdateBoardMutation } from "lib/graphql/queries/schema";
 import { DiaryDetailProps } from "types";
 
 const DiaryEditContainer = ({ id }: DiaryDetailProps) => {
   return (
     <InnerBox>
       <Profile />
-      <DiaryNewWrapper useLazyQuery={useUpdateeBoardMutation} id={id} />
+      <DiaryNewWrapper useLazyQuery={useUpdateBoardMutation} id={id} />
     </InnerBox>
   );
 };
