5cab5ff575d99bdb1e392d3f3b3be8f75318d802
henry <70426440+hyjoong@users.noreply.github.com>
2023-01-29 01:35:10 +0900

    feat: game page useCallback 함수 적용

--- components/organisms/GameWrapper/index.tsx
+++ components/organisms/GameWrapper/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 import styled from "styled-components";
 import Image from "next/image";
 import Title from "components/atoms/Title";
@@ -12,22 +12,25 @@ import LottoNumbers from "components/molecules/LottoNumbers";
 import { defaultLottoNumbers } from "constants/index";
 
 const GameWrapper = () => {
-  const [result, setResult] = useState<string>("결과");
-  const [word, setWord] = useState<string>("선풍기");
-  const [wordInput, setWordInput] = useState<string>("");
-  const [lottoList, setLottoList] = useState<string[]>(defaultLottoNumbers);
+  const [result, setResult] = useState("결과");
+  const [word, setWord] = useState("선풍기");
+  const [wordInput, setWordInput] = useState("");
+  const [lottoList, setLottoList] = useState(defaultLottoNumbers);
 
-  const handleword = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const { value } = event.target;
-    setWordInput(value);
-  };
+  const handleword = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      const { value } = event.target;
+      setWordInput(value);
+    },
+    []
+  );
 
-  const regexWord = (wordInput: string) => {
+  const regexWord = useCallback((wordInput: string) => {
     let pattern = /([^가-힣\x20])/i;
     return pattern.test(wordInput);
-  };
+  }, []);
 
-  const handleSearch = () => {
+  const handleSearch = useCallback(() => {
     if (wordInput.length === 1) {
       setResult("2글자 이상 입력해 주세요!");
       setWordInput("");
@@ -46,27 +49,30 @@ const GameWrapper = () => {
       setResult("오답입니다!");
       setWordInput("");
     }
-  };
+  }, [word, wordInput, regexWord]);
 
-  const handleOnKeyUp = (e: React.KeyboardEvent<HTMLInputElement>): void => {
-    const { value } = e.target as HTMLInputElement;
-    if (e.key === "Enter" && value.trim() !== "") {
-      handleSearch();
-    }
-  };
+  const handleOnKeyUp = useCallback(
+    (e: React.KeyboardEvent<HTMLInputElement>): void => {
+      const { value } = e.target as HTMLInputElement;
+      if (e.key === "Enter" && value !== "") {
+        handleSearch();
+      }
+    },
+    [handleSearch]
+  );
 
-  const handleMakeLottos = () => {
-    const lottos = new Array();
+  const handleMakeLottos = useCallback(() => {
+    const lottos = [];
     while (lottos.length < 6) {
       const num = Math.floor(Math.random() * 45 + 1);
-      if (lottos.indexOf(num) == -1) {
+      if (!lottos.includes(num)) {
         lottos.push(num);
       }
       lottos.push();
     }
     const sortedLottos = lottos.sort((a, b) => a - b);
     setLottoList(sortedLottos);
-  };
+  }, []);
 
   return (
     <Contents>
@@ -89,7 +95,7 @@ const GameWrapper = () => {
         </GameBox>
         <GameBox>
           <Image src={`/image/lotto.png`} width={45} height={48} alt="lotto" />
-          <Text>버튼을 누르세요.</Text>
+          <Text>버튼을 누르세요</Text>
           <LottoNumbers lottoNumbers={lottoList} />
           <Button onClick={handleMakeLottos}>추첨</Button>
         </GameBox>
